0659015786950f35d2e2d9b60dc61f754de4514d
mdallamaiqbal <bd999plus13@gmail.com>
2026-05-21 00:23:42 +0600

    add my ides api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 const uri = process.env.MONGODB_URI;
@@ -7,6 +8,9 @@ const uri = process.env.MONGODB_URI;
 const app = express();
 
 const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,7 +23,17 @@ async function run() {
   try {
    
     await client.connect();
-   
+    const db = client.db("Ideas-Vault");
+    const myIdeasCollection=db.collection("my-ideas");
+    app.get('/my-ideas', async(req,res)=>{
+      const result = await myIdeasCollection.find().toArray();
+      res.json(result)
+    })
+    app.post('/my-ideas', async(req,res)=>{
+      const myIdeasData = req.body;
+      const result = await myIdeasCollection.insertOne(myIdeasData);
+      res.json(result);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
